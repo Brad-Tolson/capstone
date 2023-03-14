7d21b0c6d34db373c3fd665836a7027a442864e5
Brad Tolson <batolson8605@yahoo.com>
2023-03-14 16:19:39 -0400

    Fourth Commit

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../Public/index.html"));
