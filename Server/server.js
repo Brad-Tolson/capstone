712c47776522b1db3b4013f7bb74b06cb0376a41
Brad Tolson <batolson8605@yahoo.com>
2023-03-14 16:06:02 -0400

    Fourth Commit

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/styles.css"));
+});
+
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/script.js"));
+});
+
 app.listen(4000, () => {
   console.log("Server listening on port 4000");
 });
